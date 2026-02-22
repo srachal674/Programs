59c5f126e9e408aede21619c6cd61bb9c2f1d342
Leigh Rachal <srachal674@gmail.com>
2026-02-21 21:38:33 -0700

    Fix grade comparison logic to use correct variables for test average and total score

--- Grade_Calc.py
+++ Grade_Calc.py
@@ -34,7 +34,7 @@ def total(test, proj, homework):
 total_score = total(test(test_ave), proj(project), homework(homework_ave))
 
 #This compares the weighted test average to the given parameter and returns the correct letter grade and number score percent.
-if test(test_ave) >=90:
+if test_ave >=90:
     print(f"Test Average = {int(test_ave + 0.5)}%")
 elif test_ave >=80:
     print(f"Test Average = {int(test_ave + 0.5)}%")
@@ -46,13 +46,13 @@ else:
     print(f"Test Average = {int(test_ave + 0.5)}%")
 
 #This prints the students total weighted grade in the class.
-if total >=90:
+if total_score >=90:
     print(f"Class Grade = A {int(total_score + 0.5)}%")
-elif total >=80:
+elif total_score >=80:
     print(f"Class Grade = B {int(total_score + 0.5)}%")
-elif total >=70:
+elif total_score >=70:
     print(f"Class Grade = C {int(total_score + 0.5)}%")
-elif total >=60:
+elif total_score >=60:
     print(f"Class Grade = D {int(total_score + 0.5)}%")
 else:
     print(f"Class Grade = F {int(total_score + 0.5)}%")
