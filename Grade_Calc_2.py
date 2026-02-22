ee7339bc3d380877850b6ce6a34a2991c8eae848
Leigh Rachal <srachal674@gmail.com>
2026-02-22 16:46:18 -0700

    Added weight max pts input

--- Grade_Calc_2.py
+++ Grade_Calc_2.py
@@ -1,26 +1,44 @@
+#This gets the total number of tests from the user.
 begin = int(input("How many test do you need to enter? "))
-test_scores = []
+
+#This stores the number of test the user set so they can be used to control the test loop and calculate the test averge.
 num_scores = begin
 
+#This stores the input test scores.
+test_scores = []
+
+#This gets the weighted test percent from the user. 
+test_weight = float(input("Enter max point value for tests: ")) / 100
+
+#This gets the weighted project percent from the user.
+project_weight = float(input("Enter max points for project: ")) / 100
+
+#This gets the weighted test percent from the user. 
+homework_weight = float(input("Enter max point value for homework: ")) / 100
+
+#This gathers the tests scores from the user based on their input number.
 for g in range (num_scores):
     grade = int(input(f"Enter test grade {g+1}: "))
     test_scores.append(grade)
 
+#This calculates their average test score using the number of test they set and the scores they entered.
 test_ave = sum(test_scores)/num_scores
 
+#This takes in the project score from the user.
 project = int(input(f"Enter project grade: "))
 
 homework_ave = int(input(f"Enter homework grade: "))
 
 # Calculate the total weighted grade
-test_points = test_ave * 0.50
-project_points = project * 0.30
-homework_points = homework_ave * 0.20
+test_points = test_ave * test_weight
+project_points = project * project_weight
+homework_points = homework_ave * homework_weight
 
 total_score = test_points + project_points + homework_points
 
-test_ave = sum(test_scores) / num_scores
-test_weighted = test_ave * .50
+"""test_ave = sum(test_scores) / num_scores
+test_weighted = test_ave * test_weight"""
+
 
 # determine letter grade
 if total_score >= 90:
